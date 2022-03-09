d71af0cc5e562c8b01be0cc4bb85cf1aafabfc21
Jose Angel <joseangeltelecom@gmail.com>
2022-03-09 09:36:36 -0500

    cambiando a login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react"
+// import React, { useEffect, useMemo } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
 import Login from "./components/Login"
 // import Details from "./components/Details"
@@ -9,21 +9,21 @@ import { ScaleLoader } from "react-spinners"
 import "./App.css"
 
 function App() {
-  const [user, setUser] = useState(null)
-  const [films, setFilms] = useState([])
-  const [character, setCharacter] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [vista, setVista] = useState([])
+  // const [user, setUser] = useState(null)
+  // const [films, setFilms] = useState([])
+  // const [character, setCharacter] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  // const [vista, setVista] = useState([])
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"))
-    const vistas = JSON.parse(localStorage.getItem("vistas"))
+  // useEffect(() => {
+  //   const user = JSON.parse(localStorage.getItem("user"))
+  //   const vistas = JSON.parse(localStorage.getItem("vistas"))
 
-    if (vistas && user) {
-      setVista(vistas)
-      setUser(user)
-    }
-  }, [])
+  //   if (vistas && user) {
+  //     setVista(vistas)
+  //     setUser(user)
+  //   }
+  // }, [])
 
   // useEffect(() => {
   //   if (user) {
@@ -41,32 +41,32 @@ function App() {
 
   // prevents the provider value from changing unless "value" or setValue changes"
 
-  const value = useMemo(
-    () => ({
-      user,
-      setUser,
-      films,
-      setFilms,
-      character,
-      setCharacter,
-      loading,
-      setLoading,
-      vista,
-      setVista,
-    }),
-    [
-      user,
-      setUser,
-      films,
-      setFilms,
-      character,
-      setCharacter,
-      loading,
-      setLoading,
-      vista,
-      setVista,
-    ]
-  )
+  // const value = useMemo(
+  //   () => ({
+  //     user,
+  //     setUser,
+  //     films,
+  //     setFilms,
+  //     character,
+  //     setCharacter,
+  //     loading,
+  //     setLoading,
+  //     vista,
+  //     setVista,
+  //   }),
+  //   [
+  //     user,
+  //     setUser,
+  //     films,
+  //     setFilms,
+  //     character,
+  //     setCharacter,
+  //     loading,
+  //     setLoading,
+  //     vista,
+  //     setVista,
+  //   ]
+  // )
 
   // const style = {
   //   position: "fixed",
@@ -81,9 +81,9 @@ function App() {
           <ScaleLoader color={"#123abc"} size={80} loading={loading} />
         </div> */}
 
-        <Routes>
-          <Route path="/" element={<Login title="Login" />} />
-{/* 
+      <Routes>
+        <Route path="/" element={<Login title="Login" />} />
+        {/* 
           {films.map((film) => (
             <Route
               path="/list"
@@ -95,7 +95,7 @@ function App() {
             />
           ))} */}
 
-          {/* {character
+        {/* {character
             ? films.map((film) => (
                 <Route
                   path={`/list/${film.episode_id}`}
@@ -103,14 +103,14 @@ function App() {
                 />
               ))
             : null} */}
-        </Routes>
+      </Routes>
       {/* </UserContext.Provider> */}
     </div>
   )
 
-  function RequireAuth({ children, redirectTo }) {
-    return user ? children : <Navigate to={redirectTo} />
-  }
+  // function RequireAuth({ children, redirectTo }) {
+  //   return user ? children : <Navigate to={redirectTo} />
+  // }
 }
 
 export default App
