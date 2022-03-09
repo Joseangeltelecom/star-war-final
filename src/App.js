b9b9e0842c8602bea6ac7d2f69bf51387f351cd4
Jose Angel <joseangeltelecom@gmail.com>
2022-03-09 11:03:31 -0500

    agregando imagenes

--- src/App.js
+++ src/App.js
@@ -1,85 +1,85 @@
-import React, { useEffect, useMemo } from "react"
+// import React, { useEffect, useMemo } from "react"
 import { Route, Routes, Navigate } from "react-router-dom"
 import Login from "./components/Login"
 // import Details from "./components/Details"
 // import List from "./components/List"
-import { useState } from "react"
-import { UserContext } from "./context/userContext"
-import { ScaleLoader } from "react-spinners"
+// import { useState } from "react"
+// import { UserContext } from "./context/userContext"
+// import { ScaleLoader } from "react-spinners"
 import "./App.css"
 
 function App() {
-  const [user, setUser] = useState(null)
-  const [films, setFilms] = useState([])
-  const [character, setCharacter] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [vista, setVista] = useState([])
+  // const [user, setUser] = useState(null)
+  // const [films, setFilms] = useState([])
+  // const [character, setCharacter] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  // const [vista, setVista] = useState([])
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"))
-    const vistas = JSON.parse(localStorage.getItem("vistas"))
+  // useEffect(() => {
+  //   const user = JSON.parse(localStorage.getItem("user"))
+  //   const vistas = JSON.parse(localStorage.getItem("vistas"))
 
-    if (vistas && user) {
-      setVista(vistas)
-      setUser(user)
-    }
-  }, [])
+  //   if (vistas && user) {
+  //     setVista(vistas)
+  //     setUser(user)
+  //   }
+  // }, [])
 
-  useEffect(() => {
-    if (user) {
-      const filmss = JSON.parse(localStorage.getItem("films"))
-      setFilms(filmss)
-    }
-  }, [user])
+  // useEffect(() => {
+  //   if (user) {
+  //     const filmss = JSON.parse(localStorage.getItem("films"))
+  //     setFilms(filmss)
+  //   }
+  // }, [user])
 
-  useEffect(() => {
-    if (user) {
-      const characters = JSON.parse(localStorage.getItem("characters"))
-      setCharacter(characters)
-    }
-  }, [user])
+  // useEffect(() => {
+  //   if (user) {
+  //     const characters = JSON.parse(localStorage.getItem("characters"))
+  //     setCharacter(characters)
+  //   }
+  // }, [user])
 
   // prevents the provider value from changing unless "value" or setValue changes"
 
-  const value = useMemo(
-    () => ({
-      user,
-      setUser,
-      films,
-      setFilms,
-      character,
-      setCharacter,
-      loading,
-      setLoading,
-      vista,
-      setVista,
-    }),
-    [
-      user,
-      setUser,
-      films,
-      setFilms,
-      character,
-      setCharacter,
-      loading,
-      setLoading,
-      vista,
-      setVista,
-    ]
-  )
+  // const value = useMemo(
+  //   () => ({
+  //     user,
+  //     setUser,
+  //     films,
+  //     setFilms,
+  //     character,
+  //     setCharacter,
+  //     loading,
+  //     setLoading,
+  //     vista,
+  //     setVista,
+  //   }),
+  //   [
+  //     user,
+  //     setUser,
+  //     films,
+  //     setFilms,
+  //     character,
+  //     setCharacter,
+  //     loading,
+  //     setLoading,
+  //     vista,
+  //     setVista,
+  //   ]
+  // )
 
-  const style = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  }
+  // const style = {
+  //   position: "fixed",
+  //   top: "50%",
+  //   left: "50%",
+  //   transform: "translate(-50%, -50%)",
+  // }
   return (
     <div className="App h-100">
-      <UserContext.Provider value={value}>
-        <div style={style}>
+      {/* <UserContext.Provider value={value}> */}
+        {/* <div style={style}>
           <ScaleLoader color={"#123abc"} size={80} loading={loading} />
-        </div>
+        </div> */}
 
         <Routes>
           <Route path="/" element={<Login title="Login" />} />
@@ -104,13 +104,13 @@ function App() {
               ))
             : null} */}
         </Routes>
-      </UserContext.Provider>
+      {/* </UserContext.Provider> */}
     </div>
   )
 
-  function RequireAuth({ children, redirectTo }) {
-    return user ? children : <Navigate to={redirectTo} />
-  }
+  // function RequireAuth({ children, redirectTo }) {
+  //   return user ? children : <Navigate to={redirectTo} />
+  // }
 }
 
 export default App
